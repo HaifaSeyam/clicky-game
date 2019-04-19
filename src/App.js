edf868253df43b6414d5331c4eb6cf267aacac21
HaifaSeyam <h.abuamro@hotmail.com>
2019-04-19 09:52:34 -0500

    header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
 
 
 class App extends React.Component {
 
+  state = {
+    score: 0,
+    highscore: 0
+  }
+
     render() {
-        return (<div> Initail files created </div>);
+        return (<Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>);
       }
   
 }
