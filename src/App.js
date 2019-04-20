86f86acdea9b8cbc0f14b87b3a7cd4b9bc651925
HaifaSeyam <h.abuamro@hotmail.com>
2019-04-19 20:21:49 -0500

    clicked images check done - score and msg updated accordingly - topscore not done yet

--- src/App.js
+++ src/App.js
@@ -20,9 +20,40 @@ class App extends React.Component {
     this.setState({
       butterflies: butterflies
     });
+
+    //Loop through the butterflies array, and check 
+    //if a specific element.clicked is true 
+    //then change the score to zero and the msg to "incorrect guess - try again!"
+
+    const selectedElement = this.state.butterflies.find(element => element.id === id);
+    console.log(selectedElement)
+
+    if (selectedElement.clicked) {
+      this.setState({
+        score: 0,
+        msg: "Incorrect Guess - Try Again!"
+      })
+
+      selectedElement.clicked = false;
+
+
+    } else {
+      this.setState({
+        score: this.state.score + 1,
+        msg: "Correct Guess - Keep Going!"
+      })
+      
+      selectedElement.clicked = true;
+    }
+
+    //if this element.clicked is false
+    //change it to true
+    //add +1 to score
+    //check if topscore < 12 , if true add +1 , if false keep the current value
+    //change the msg to "correct guess - keep going!"
     
   };
-  
+
     render() {
         return (
           <React.Fragment>
