2ed6ac26d00e8d68c20c8c144b457174932fc03d
HaifaSeyam <h.abuamro@hotmail.com>
2019-04-19 22:37:04 -0500

    app finalized -- with bug to be fixed

--- src/App.js
+++ src/App.js
@@ -21,37 +21,37 @@ class App extends React.Component {
       butterflies: butterflies
     });
 
-    //Loop through the butterflies array, and check 
-    //if a specific element.clicked is true 
-    //then change the score to zero and the msg to "incorrect guess - try again!"
-
     const selectedElement = this.state.butterflies.find(element => element.id === id);
     console.log(selectedElement)
 
     if (selectedElement.clicked) {
+
+      selectedElement.clicked = false;
+
       this.setState({
         score: 0,
         msg: "Incorrect Guess - Try Again!"
       })
 
-      selectedElement.clicked = false;
-
-
     } else {
+
+      selectedElement.clicked = true;
+
       this.setState({
         score: this.state.score + 1,
         msg: "Correct Guess - Keep Going!"
-      })
-      
-      selectedElement.clicked = true;
-    }
+      });
 
-    //if this element.clicked is false
-    //change it to true
-    //add +1 to score
-    //check if topscore < 12 , if true add +1 , if false keep the current value
-    //change the msg to "correct guess - keep going!"
-    
+        if(this.state.score === this.state.topscore) {
+          
+            this.setState({
+              topscore: this.state.topscore + 1,
+            });
+      
+        }
+
+      }
+      
   };
 
     render() {
