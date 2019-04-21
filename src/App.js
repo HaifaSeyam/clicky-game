ac12a0fb8ad88de36ad9062bd968c640a02797e4
HaifaSeyam <h.abuamro@hotmail.com>
2019-04-21 00:44:52 -0500

    bug fixed finally - restart the game after winning has been done

--- src/App.js
+++ src/App.js
@@ -16,45 +16,76 @@ class App extends React.Component {
 
   imgSelect = id => {
 
-    butterflies.sort(() => Math.random() - 0.5);
+    const sortedButterflies = this.state.butterflies.sort(() => Math.random() - 0.5);
     this.setState({
-      butterflies: butterflies
+      butterflies: sortedButterflies
     });
 
     const selectedElement = this.state.butterflies.find(element => element.id === id);
     console.log(selectedElement)
 
     if (selectedElement.clicked) {
+      //This code for some reason does not work as required
+      // const reset = this.state.butterflies.map(element => {
+      //  return {...element, clicked: false}
+      // });
 
-      //I need to change the clicked property to false for all of the array elements
-      const reset = this.state.butterflies.map(element => {
-       return {...element, clicked: false}
-      })
+      //Creating a new array with the same values of the state array --- this is done to over come the issue of mutating
+      var reset = this.state.butterflies;
 
+      //Set the clicked value to false for all elements
+      for(let i = 0; i < reset.length; i++) {
+        reset[i].clicked = false;
+      }
+
+      //console.log("RESET", reset);
+
+      //Set the butterflies array of the state to the reset array
       this.setState({
         score: 0,
         butterflies: reset,
         msg: "Incorrect Guess - Try Again!"
       })
 
-    } else {
+      //console.log("New Butterflies", this.state.butterflies);
 
-      selectedElement.clicked = true;
+    } 
+    else {
+        selectedElement.clicked = true;
 
-      this.setState({
-        score: this.state.score + 1,
-        msg: "Correct Guess - Keep Going!"
-      });
-
-        if(this.state.score === this.state.topscore) {
-          
+        if (this.state.score < 11) {
+          //console.log("SCORE < 12 before adding 1", this.state.score);
             this.setState({
-              topscore: this.state.topscore + 1,
+              score: this.state.score + 1,
+              msg: "Correct Guess - Keep Going!"
             });
-      
+
+            if(this.state.score === this.state.topscore) {
+              this.setState({
+                topscore: this.state.topscore + 1,
+              });
+            }
+            //console.log("SCORE < 12 after adding 1", this.state.score);
+            console.log("ORIGINAL", this.state.butterflies);
+        } else if (this.state.score === 11){
+
+          //console.log("SCORE === 12 before clearing", this.state.score);
+          const clear = this.state.butterflies.map(element => {
+            return {...element, clicked: false}
+           });
+
+           this.setState({
+             score: 0,
+             topscore: 0,
+             butterflies: clear,
+             msg: "You have won! Click on any image to start over ..."
+           });
+
+           console.log("CLEAR: ", clear)
+           //console.log("SCORE === 12 after clearing", this.state.score);
         }
 
-      }
+       }
       
   };
 
